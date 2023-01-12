9980fa69b810534839b2da59a8d865cc1baa6f87
Jason Chuang <chuangtcee@gmail.com>
2023-01-12 09:09:46 +0800

    fix primary key constraint issue in JPA

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO product(product_id,name,brand,madein,price) VALUES(1,'car','Toyota','Japan',700000);
-INSERT INTO product(product_id,name,brand,madein,price) VALUES(2,'car','Honda','Japan',600000);
+INSERT INTO product(product_id,name,brand,madein,price) VALUES(default,'car','Toyota','Japan',700000);
+INSERT INTO product(product_id,name,brand,madein,price) VALUES(default,'car','Honda','Japan',600000);
 --password: '123'
-INSERT INTO users(user_id,username,password,enabled) VALUES(1,'chuangtc@gmail.com','$2y$10$xkUJUC3uFdlmZzKVJDug.O27Sx3P2DQcFoQrXgqXB.tCdyAL6MD/O',true);
-INSERT INTO users(user_id,username,password,enabled) VALUES(2,'chuangtc2@gmail.com','$2y$10$xkUJUC3uFdlmZzKVJDug.O27Sx3P2DQcFoQrXgqXB.tCdyAL6MD/O',true);
-INSERT INTO roles(role_id,name) VALUES(1,'role1');
-INSERT INTO roles(role_id,name) VALUES(2,'role2');
+INSERT INTO users(user_id,username,password,enabled) VALUES(default,'chuangtc@gmail.com','$2y$10$xkUJUC3uFdlmZzKVJDug.O27Sx3P2DQcFoQrXgqXB.tCdyAL6MD/O',true);
+INSERT INTO users(user_id,username,password,enabled) VALUES(default,'chuangtc2@gmail.com','$2y$10$xkUJUC3uFdlmZzKVJDug.O27Sx3P2DQcFoQrXgqXB.tCdyAL6MD/O',true);
+INSERT INTO roles(role_id,name) VALUES(default,'role1');
+INSERT INTO roles(role_id,name) VALUES(default,'role2');
